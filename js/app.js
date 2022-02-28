f3a8cf49dbc7cfb179326e2391c73e123de3abee
Deceit-87 <manuelfreund318@gmail.com>
2022-02-28 18:06:48 +0100

    aggiunto v-for preview msg

---
+++ js/app.js
@@ -0,0 +1,69 @@
+
+const app = new Vue({
+ el: '#app',
+ data: {
+     
+    contatti: [
+        
+            {
+                name: 'Michele',
+                avatar: '_1',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                text: 'Hai portato a spasso il cane?',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                text: 'Ricordati di dargli da mangiare',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 16:15:22',
+                text: 'Tutto fatto!',
+                status: 'received'
+                }
+            ]},
+            {
+                name: 'Mario',
+                avatar: '_1',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                text: 'Serio?',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                text: 'Ricordati di dargli da mangiare',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 16:15:22',
+                text: 'Tutto fatto!',
+                status: 'received'
+                }
+            ]}
+   
+   
+   
+            ],
+
+                
+
+       },
+methods:{
+}
+   
+})
+                   
+    
+
+
+
+
+
+
