885fcf891f142cf27381c64b131d48383637ff67
Deceit-87 <manuelfreund318@gmail.com>
2022-02-28 18:55:13 +0100

    aggiunte array

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const app = new Vue({
         
             {
                 name: 'Michele',
-                avatar: '_1',
+                avatar: 'https://picsum.photos/id/1/200/300',
                 visible: true,
                 messages: [
                 {
@@ -28,7 +28,7 @@ const app = new Vue({
             ]},
             {
                 name: 'Mario',
-                avatar: '_1',
+                avatar: 'https://picsum.photos/id/2/200/300',
                 visible: true,
                 messages: [
                 {
@@ -45,7 +45,28 @@ const app = new Vue({
                 date: '10/01/2020 16:15:22',
                 text: 'Tutto fatto!',
                 status: 'received'
-                }
+                }]
+            },
+                {
+                    name: 'Ale',
+                    avatar: 'https://picsum.photos/id/3/200/300',
+                    visible: true,
+                    messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    text: 'Ci sei stasera?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    text: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    text: 'Tutto fatto!',
+                    status: 'received'
+                    }
             ]}
    
    
